d7172d8c8bb34c7ec8c5946ff8cc76dd953f18aa
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-11-23 19:50:43 -0300

    Update executar Linux.sh

--- executar Linux.sh
+++ executar Linux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "Executando os programas";
-"Merge Sort/parallel.out" > "Merge Sort/saida_parallel.txt";
-"Merge Sort/serial.out" > "Merge Sort/saida_serial.txt";
-"Parallel Sort/parallel.out" > "Parallel Sort/saida_parallel.txt";
-"Quick Sort/serial.out" > "Quick Sort/saida_serial.txt";
-"Selection Sort/parallel1.out" > "Selection Sort/saida_parallel1.txt";
-"Selection Sort/parallel2.out" > "Selection Sort/saida_parallel2.txt";
-"Selection Sort/serial.out" > "Selection Sort/saida_serial.txt";
-"Shell Sort/parallel.out" > "Shell Sort/saida_parallel.txt";
-"Shell Sort/serial.out" > "Shell Sort/saida_serial.txt";
-"Odd Even Sort/serial.out" > "Odd Even Sort/saida_serial.txt";
+time_timeout="30m";
+
+timeout $time_timeout "Merge Sort/parallel.out" > "Merge Sort/saida_parallel.txt";
+timeout $time_timeout "Merge Sort/serial.out" > "Merge Sort/saida_serial.txt";
+timeout $time_timeout "Parallel Sort/parallel.out" > "Parallel Sort/saida_parallel.txt";
+timeout $time_timeout "Quick Sort/serial.out" > "Quick Sort/saida_serial.txt";
+timeout $time_timeout "Selection Sort/parallel1.out" > "Selection Sort/saida_parallel1.txt";
+timeout $time_timeout "Selection Sort/parallel2.out" > "Selection Sort/saida_parallel2.txt";
+timeout $time_timeout "Selection Sort/serial.out" > "Selection Sort/saida_serial.txt";
+timeout $time_timeout "Shell Sort/parallel.out" > "Shell Sort/saida_parallel.txt";
+timeout $time_timeout "Shell Sort/serial.out" > "Shell Sort/saida_serial.txt";
+timeout $time_timeout "Odd Even Sort/serial.out" > "Odd Even Sort/saida_serial.txt";
 read -p "Digite qualquer tecla para continuar";
